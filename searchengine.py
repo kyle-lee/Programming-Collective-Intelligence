5be13da708d1b9bc0d75056195634508383ee6dd
John Anderson <sontek@gmail.com>
2012-01-21 13:49:23 -0600

    Updated searchengyne.py

--- searchengine.py
+++ searchengine.py
@@ -1,19 +1,20 @@
 import urllib2
 from BeautifulSoup import *
 from urlparse import urljoin
+from pysqlite2 import dbapi2 as sqlite
 
 ignorewords = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 
 
 class crawler:
     def __init__(self, dbname):
-        pass
+        self.con = sqlite.connect(dbname)
 
     def __del__(self):
-        pass
+        self.con.close()
 
     def dbCommit(self):
-        pass
+        self.con.commit()
 
     # Auxilliary function for getting an entry id and adding it if it is not
     # present.
@@ -25,7 +26,16 @@ class crawler:
         print 'Indexing %s' % url
 
     def getTextOnly(self, soup):
-        return None
+        v = soup.string
+        if v is None:
+            c = soup.contents
+            resulttext = ''
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+            return resulttext
+        else:
+            return v.strip()
 
     def separateWords(self, text):
         return None
@@ -64,4 +74,14 @@ class crawler:
             pages = newPages
 
     def createIndexTables(self):
-        pass
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid, wordid, location)')
+        self.con.execute('create table link(fromid integer, toid integer)')
+        self.con.execute('create table linkwords(wordid, linkid)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
+        self.dbCommit()
